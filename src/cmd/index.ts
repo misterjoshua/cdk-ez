93acbf62af4dac403aaee704216e76efe79a9305
Josh Kellendonk <joshkellendonk@gmail.com>
2019-12-29 02:08:22 -0700

    foo

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -1,5 +1,9 @@
 import { Sade } from "sade";
 
+export const registerNotImplementedCommand = (prog: Sade, command: string) => {
+  prog.command(command).action(() => { console.error(`${command} is not implemented yet`) });
+}
+
 export const registerBuildCommand = (prog: Sade) => {
   prog
     .command("build")
@@ -11,4 +15,8 @@ export const registerBuildCommand = (prog: Sade) => {
 
 export const registerCommands = (prog: Sade) => {
   registerBuildCommand(prog);
+
+  registerNotImplementedCommand(prog, 'test');
+  registerNotImplementedCommand(prog, 'lint');
+  registerNotImplementedCommand(prog, 'deploy');
 };
