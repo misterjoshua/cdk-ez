0d8a787056a05d2aa9042fa9ab9b291868b83251
Josh Kellendonk <joshkellendonk@gmail.com>
2019-12-29 11:52:02 -0700

    Cleanup

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -1,23 +1,33 @@
 import { Sade } from 'sade';
 
 export const registerNotImplementedCommand = (prog: Sade, command: string): void => {
-    prog.command(command).action(() => {
-        console.error(`${command} is not implemented yet`);
-    });
+  prog.command(command).action(() => {
+    console.error(`${command} is not implemented yet`);
+  });
 };
 
 export const registerBuildCommand = (prog: Sade): void => {
-    prog.command('build')
-        .describe('it runs build')
-        .action(() => {
-            console.log('Hello');
-        });
+  prog
+    .command('build <app>')
+    .describe('it runs build')
+    .action(() => {
+      console.log('Build');
+    });
+};
+
+export const registerDeployCommand = (prog: Sade): void => {
+  prog
+    .command('deploy <app>')
+    .describe('it runs deploy')
+    .action(() => {
+      console.log('Deploy');
+    });
 };
 
 export const registerCommands = (prog: Sade): void => {
-    registerBuildCommand(prog);
+  registerBuildCommand(prog);
 
-    registerNotImplementedCommand(prog, 'test');
-    registerNotImplementedCommand(prog, 'lint');
-    registerNotImplementedCommand(prog, 'deploy');
+  registerNotImplementedCommand(prog, 'test');
+  registerNotImplementedCommand(prog, 'lint');
+  registerNotImplementedCommand(prog, 'deploy');
 };
