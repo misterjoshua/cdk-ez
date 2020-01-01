9259ad23ba50af4c59d8497535cad4f7fcf44368
Josh Kellendonk <joshkellendonk@gmail.com>
2019-12-31 21:34:26 -0700

    Add auto-generate for editorconfig

--- src/config/installLocalConfig.ts
+++ src/config/installLocalConfig.ts
@@ -1,7 +1,12 @@
 import { promisify } from "util";
 import fs from "fs";
 import { getTypescriptConfig } from "../opinions/typescript";
-import { shouldAutoGenerateFile, createJsonFile } from "./autoGenerate";
+import {
+  shouldAutoGenerateFile,
+  createJsonFile,
+  createIniFile
+} from "./autoGenerate";
+import { getEditorConfig } from "../opinions/editorconfig";
 
 async function installTypescriptConfig(): Promise<void> {
   const tsconfig = "./tsconfig.json";
@@ -13,9 +18,20 @@ async function installTypescriptConfig(): Promise<void> {
   }
 }
 
+async function installEditorConfig(): Promise<void> {
+  const editorconfig = "./.editorconfig";
+
+  if (await shouldAutoGenerateFile(editorconfig)) {
+    const config = await getEditorConfig();
+    const configFile = createIniFile(config);
+    await promisify(fs.writeFile)(editorconfig, configFile);
+  }
+}
+
 export async function installLocalConfig(): Promise<void> {
   await Promise.all([
-    installTypescriptConfig()
+    installTypescriptConfig(),
+    installEditorConfig()
     //
   ]);
 }
---
+++ src/opinions/editorconfig.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
+export type EditorConfigString<Options> = Options | undefined;
+export type EditorConfigBoolean = boolean | undefined;
+export type EditorConfigIndent = number | undefined;
+
+export interface EditorConfigSection {
+  charset?: EditorConfigString<
+    "latin1" | "utf-16be" | "utf-16le" | "utf-8" | "utf-8-bom"
+  >;
+  end_of_line?: EditorConfigString<"cr" | "lf" | "crlf">;
+  indent_size?: EditorConfigIndent;
+  indent_style?: EditorConfigString<"space" | "tab">;
+  insert_final_newline?: EditorConfigBoolean;
+  root?: EditorConfigBoolean;
+  tab_width?: EditorConfigIndent;
+  trim_trailing_whitespace?: EditorConfigBoolean;
+}
+
+export interface EditorConfig {
+  [x: string]: EditorConfigSection;
+}
+
+export async function getEditorConfig(): Promise<EditorConfig> {
+  return {
+    "*": {
+      charset: "utf-8",
+      indent_size: 2,
+      indent_style: "space",
+      insert_final_newline: true,
+      trim_trailing_whitespace: true
+    }
+  };
+}
